3a4f66b6745341b4de8c1fcf7fe980dccc824656
Jorn3355 <RealJornShyti@gmail.com>
2024-07-13 16:21:58 +0200

    Final patch, program can now generates the grid with no overflow and also it can reset the whole container back to normal, its not responsive as that would be hard to implement. Changing anything on the techincal side is unneeded however it could really use a whole css makeover

--- Etch.js
+++ Etch.js
@@ -1,19 +1,35 @@
 const container = document.querySelector(".container")
-const length = document.querySelector("#length")
 const printBtn = document.querySelector("#print")
+const resetBtn = document.querySelector("#reset")
+const contWidth = container.clientWidth
+const contHeight = container.clientHeight
 
 printBtn.addEventListener("click", () => {
-    let boxCount = parseInt(length.value)
-    if (Number.isInteger(boxCount)){
-        for(let i = 0; i <= boxCount**2; i++){
-            let newItem = document.createElement("div")
-            newItem.classList.add("box")
-            container.appendChild(newItem)
-        }
+    const boxCount = parseInt(prompt("Choose your grid size all the way up to 64"))
+    if (boxCount > 64){
+        alert("The maximum number of boxes we can generate is 64.")
     } else {
-        alert("Moron, input a regular number.")
-    
-}})
+        if (Number.isInteger(boxCount)){
+            for(let i = 1; i <= boxCount*boxCount; i++){
+                let newItem = document.createElement("div")
+                newItem.classList.add("box")
+                newItem.setAttribute("style", `width:${contWidth/boxCount}px;`)
+                container.appendChild(newItem)
+            }
+        } else {
+            alert("Moron, input a regular number.")
+        }
+    }
+})
 
+container.addEventListener("mouseover", (item) => {
+    if (item.target.matches('.box')) {
+        item.target.classList.add('active')
+    }
+})
 
-
+resetBtn.addEventListener("click", () => {
+    while(container.firstChild){
+        container.removeChild(container.lastChild)
+    }
+})
